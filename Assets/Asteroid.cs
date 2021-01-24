564dd08ce28f6736c12600c1bb251b7478dc8ad0
kagolli <kevinagolli98@gmail.com>
2021-01-24 18:42:39 -0500

    finished tutorial

--- Assets/Asteroid.cs
+++ Assets/Asteroid.cs
@@ -6,11 +6,13 @@ public class Asteroid : MonoBehaviour
 {
 
     public GameObject deathExplosion;
+    public int pointValue;
+    public AudioClip deathKnell;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointValue = 10;
     }
 
     // Update is called once per frame
@@ -29,8 +31,18 @@ public class Asteroid : MonoBehaviour
         plane. as a result, since we are looking down the Y axis, we
         rotate the particle system so that it flys in the right way.
         */
+        // Instantiate(deathExplosion, gameObject.transform.position, Quaternion.AngleAxis(-90, Vector3.right) );
+        // Destroy (gameObject);
+        AudioSource.PlayClipAtPoint(deathKnell, gameObject.transform.position );
+
         Instantiate(deathExplosion, gameObject.transform.position, Quaternion.AngleAxis(-90, Vector3.right) );
+
+        GameObject obj = GameObject.Find("GlobalObject");
+        Global g = obj.GetComponent<Global>();
+        g.score += pointValue;
+        
         Destroy (gameObject);
+
     }
 
 }
